6a030f41fba756f6621f79745de0b1ed736116b4
SilMon <weiss-romano@web.de>
2020-04-30 15:09:28 +0200

    Add files via upload

---
+++ Bacteria_counter.java
@@ -0,0 +1,118 @@
+import ij.ImagePlus;
+import ij.process.AutoThresholder;
+import ij.process.AutoThresholder.Method;
+import ij.process.ColorProcessor;
+import ij.process.ByteProcessor;
+import ij.gui.DialogListener;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.ExtendedPlugInFilter;
+import ij.plugin.filter.PlugInFilterRunner;
+import ij.process.ImageProcessor;
+import ij.plugin.filter.GaussianBlur;
+import ij.plugin.filter.RankFilters;
+import ij.IJ;
+import java.awt.AWTEvent;
+import java.lang.Math;
+import java.util.Arrays;
+
+
+public class Bacteria_counter implements ExtendedPlugInFilter, DialogListener{
+	private final int FLAGS = DOES_RGB | PARALLELIZE_STACKS;
+	private boolean preview = true;
+	private double bacteriaHeight, pixelsPerBacterium;
+	private boolean stretchContrast;
+
+	public int setup(String args, ImagePlus im){
+		// Define variables
+		bacteriaHeight = 1;
+		double bacteriumArea = 1.5;
+		int binning = 2;
+		double pixelSize20 = 0.172;
+		double miM2Pixels = 1.0d / (binning * binning * pixelSize20 * pixelSize20);
+		pixelsPerBacterium = bacteriumArea * miM2Pixels;
+		stretchContrast = true;
+		return FLAGS;
+	}
+	
+	public void run(ImageProcessor ip){
+		// Cast ip to ColorProcessor and get green channel
+		ColorProcessor cp = (ColorProcessor) ip;
+		ByteProcessor green = cp.getChannel(2, null);
+		// Get treshhold value
+		AutoThresholder thresh = new AutoThresholder();
+		double threshold = thresh.getThreshold(Method.Li, green.getHistogram(256));
+		double theoArea = 0;
+		double maxHeight = 0;
+		// Minimal height is 0
+		int minPix = 0;
+		int maxPix = 0;
+		// Change value of pixels according to threshold
+		for (int v = 0; v < cp.getHeight(); v++){
+			for (int u = 0; u < cp.getWidth(); u++){
+				// Get pixel value
+				int value = green.get(u, v);
+				if (value > threshold){
+					theoArea += value / threshold;
+					double val = value / (threshold * bacteriaHeight);
+					// Check if val is larger than the maxHeight
+					maxHeight = (maxHeight < val) ? val : maxHeight;
+					// Construct a new RGB pixel
+					int pixVal = (int)Math.round(val);
+					maxPix = (maxPix < pixVal) ? pixVal : maxPix;
+					int newPix = pixVal << 16 | pixVal << 8 | pixVal;
+					ip.set(u, v, newPix);
+				}else{
+					ip.set(u, v, 0);
+				}
+			}
+		}
+		if (stretchContrast){
+			// Calculate ratio of 255 to min max distance
+			double ratio = 255.0 / (maxPix - minPix);
+			//IJ.log(String.format("Min: %d Max: %d Ratio: %f", minPix, maxPix, ratio));
+			for (int v = 0; v < cp.getHeight(); v++){
+				for (int u = 0; u < cp.getWidth(); u++){
+					// Stretch contrast according to ratio
+					int pix = (int)(((ip.get(u, v) >> 16) & 0xFF) * ratio);
+					int newPix = pix << 16 | pix << 8 | pix;
+					ip.set(u, v, newPix);
+				}
+			}
+		}
+		// Log results
+		IJ.log(String.format("Approximate Bacteria Number: %d", Math.round(theoArea / pixelsPerBacterium)));
+		IJ.log(String.format("Max. Height: %f", maxHeight));
+		IJ.log(String.format("Theoretical Area: %f", theoArea));
+		IJ.log("");
+	}
+	
+	public int showDialog(ImagePlus imp, String Command, PlugInFilterRunner pfr){
+		// Create interface dialog
+		GenericDialog gd = new GenericDialog("BacteriaCounter");
+		gd.addMessage("Set up the analysis parameters:");
+		gd.addNumericField("Bacteria Height", bacteriaHeight, 4, 6, "units");
+		gd.addNumericField("Pixels per Bacterium", pixelsPerBacterium, 4, 6, "pixels");
+		gd.addCheckbox("Stretch Contrast", true);
+		gd.addPreviewCheckbox(pfr, "Preview");
+		gd.addDialogListener(this);
+		gd.showDialog();
+		
+		if(gd.wasCanceled() ||!dialogItemChanged(gd, null)){
+			return DONE;
+		}
+		return IJ.setupDialog(imp, FLAGS);
+	}
+	
+	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e){
+		// React to User Input
+		bacteriaHeight = gd.getNextNumber();
+		pixelsPerBacterium = gd.getNextNumber();
+		stretchContrast = gd.getNextBoolean();
+		return true;
+	}
+	
+	public void setNPasses(int nPasses){
+		//Has to be implemented for ExtendedPlugInFilter
+	}
+	
+}
